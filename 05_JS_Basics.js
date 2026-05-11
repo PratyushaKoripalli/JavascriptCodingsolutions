d7cc67e9220af74fed3bf8d45135aabcca7a577d
Pratyusha Koripalli <pratyusha@example.com>
2026-05-11 22:34:51 +0530

    Add JS basics and Chapter 02 identifier rules

---
+++ 05_JS_Basics.js
@@ -0,0 +1,8 @@
+var v = 10;
+console.log(v);
+
+var v = 12;
+console.log(v);
+
+let x = 12;
+console.log(x);
---
+++ Chapter_02_Javascript_Concepts/06_Identifier_Rules.js
@@ -0,0 +1,11 @@
+var _ = 10;
+console.log(_);
+
+var $abc = 12;
+console.log($abc);
+
+var abc123 = 15;
+console.log(abc123);
+
+var text = "Hello World";
+console.log(text);
